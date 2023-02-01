3d6811c60cf65644ace518d40149608b0d693b57
rishi <rks83601@gmail.com>
2023-02-01 10:39:20 +0545

    commited

--- src/components/featuredProperties/FeaturedPropertyCard.jsx
+++ src/components/featuredProperties/FeaturedPropertyCard.jsx
@@ -12,17 +12,19 @@ const FeaturedPropertyCard = ({ featured }) => {
     <>
       <div className="box shadow p-2 flex flex-col gap-2 ">
         <div className="img rounded-lg">
-          <img src={cover} className="rounded-lg" alt="" />
+          <img src={cover} className="rounded-lg max-w-[400px]" alt="" />
         </div>
         <div className={`${styled.text} text mt-[0.8rem]  `}>
           <div
-            className={`${styled.category} ${styled.flex} flex  items-center justify-between category`}>
+            className={`${styled.category} ${styled.flex} flex  items-center justify-between category`}
+          >
             <span
               className="mt-0 text-sm"
               style={{
                 background: category === "For Sale" ? "#25b5791a" : "#ff98001a",
                 color: category === "For Sale" ? "#25b579" : "#ff9800",
-              }}>
+              }}
+            >
               {category}
             </span>
             {isLiked ? (
@@ -35,7 +37,8 @@ const FeaturedPropertyCard = ({ featured }) => {
           <p className="text-xs text-gray-400">{location}</p>
         </div>
         <div
-          className={`${styled.button} ${styled.flex} button flex items-center justify-between`}>
+          className={`${styled.button} ${styled.flex} button flex items-center justify-between`}
+        >
           <div>
             <button className=" px-2 py-[0.2rem] text-center font-light">
               {price}
