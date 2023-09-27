f89264130236637bd4c990d278f22714df7606a4
Rishikesh Sharma <wrongdirection72@gmail.com>
2023-09-27 17:42:49 +0545

    debugging 11

--- src/pages/middleware.js
+++
@@ -1,9 +0,0 @@
-// import { NextResponse } from "next/server";
-// export function middleware(request) {
-//   const url = request.nextUrl.clone();
-//   console.log(url);
-//   if (url.pathname === "/dashboard") {
-//     url.pathname = "/dashboard/profile";
-//     return NextResponse.redirect(url);
-//   }
-// }
