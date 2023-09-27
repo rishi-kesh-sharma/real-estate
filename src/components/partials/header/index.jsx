594a6b3c028e698a9d50b687571f8ab5a122556e
Rishikesh Sharma <wrongdirection72@gmail.com>
2023-09-27 17:28:06 +0545

    debugging 9

--- src/components/partials/header/index.jsx
+++ src/components/partials/header/index.jsx
@@ -43,7 +43,6 @@ const Navbar = () => {
   useEffect(() => {
     // LISTENING TO RESIZE EVENT
     window.addEventListener("resize", handleResize);
-    handleResize();
   }, []);
 
   return (
--- src/components/utils/index.js
+++ src/components/utils/index.js
@@ -4,8 +4,7 @@ import Toast from "./Toast";
 export const logoutFunctionality = async (removeCookie, router) => {
   try {
     const res = await logoutUser();
-    console.log(res);
-    router.reload(window.location.href);
+    // router.reload(window.location.href);
     removeTokenFromLocalStorage();
     removeCookie("token");
     router.reload("/");
