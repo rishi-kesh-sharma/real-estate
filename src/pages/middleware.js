1f7a3cf2a869e15a7b5816cae99f10aacbca743c
Rishikesh Sharma <wrongdirection72@gmail.com>
2023-09-27 17:40:42 +0545

    debugging 10

--- src/pages/middleware.js
+++ src/pages/middleware.js
@@ -1,9 +1,9 @@
-import { NextResponse } from "next/server";
-export function middleware(request) {
-  const url = request.nextUrl.clone();
-  console.log(url);
-  if (url.pathname === "/dashboard") {
-    url.pathname = "/dashboard/profile";
-    return NextResponse.redirect(url);
-  }
-}
+// import { NextResponse } from "next/server";
+// export function middleware(request) {
+//   const url = request.nextUrl.clone();
+//   console.log(url);
+//   if (url.pathname === "/dashboard") {
+//     url.pathname = "/dashboard/profile";
+//     return NextResponse.redirect(url);
+//   }
+// }
